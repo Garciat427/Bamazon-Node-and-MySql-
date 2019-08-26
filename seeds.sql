9912d95d84391e52e7cb95cf69fe45522e7e7288
Troy Garcia <tg1212@outlook.com>
2019-08-26 17:33:39 -0400

    Added add dept function to supervisor program

--- seeds.sql
+++ seeds.sql
@@ -14,7 +14,7 @@ VALUES ("Animal Crackers", "Food", 1.25, 30);
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("White TShirt", "Clothing", 5.00, 10);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
+INSERT INTO products (product_name, department_name, price, stck_quantity)
 VALUES ("Black Hoodie", "Clothing", 15.00, 1);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
@@ -36,7 +36,7 @@ VALUES ("Supplies", 1000, 2000);
 INSERT INTO departments (department_name, over_head_costs, product_sales)
 VALUES ("Food", 5000, 7500);
 
-INSERT INTO departments (department_name, over_head_costs, product_sales)
+INSERT INTO departments (department_name, over_head_costs, C)
 VALUES ("Clothing", 5000, 7500);
 
 INSERT INTO departments (department_name, over_head_costs, product_sales)
